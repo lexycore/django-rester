4fed91b1ceb3d5cfce82bbb9c9e44974580310c5
zili <zili.tnd@gmail.com>
2018-03-13 21:55:40 +0300

    singleton metaclass added url_mapper fixes

--- django_rester/swagger.py
+++ django_rester/swagger.py
@@ -1,6 +1,8 @@
 from .url_mapper import UrlMapper
+from .singleton import Singleton
 
-class SwaggerGenerator:
+
+class SwaggerGenerator(metaclass=Singleton):
 
     def __init__(self):
         self.url_view_map = UrlMapper().urls_class_map
--- django_rester/url_mapper.py
+++ django_rester/url_mapper.py
@@ -2,17 +2,18 @@ from django.conf.urls import RegexURLPattern, RegexURLResolver
 from django.core import urlresolvers
 
 from django_rester.views import BaseAPIView
+from .singleton import Singleton
 
 
-class UrlMapper:
+class UrlMapper(metaclass=Singleton):
+    urls_class_map = {}
 
     def __init__(self):
         urls = urlresolvers.get_resolver()
-        self.urls_class_map = {}
         self._build_map(urls)
 
     @staticmethod
-    def _import_by_path(name):
+    def _filter_rester_views(name):
         _cls = None
         components = name.split('.')
         module = __import__(components[0])
@@ -25,18 +26,12 @@ class UrlMapper:
                 continue
         return _cls
 
-    @staticmethod
-    def _if_none(value):
-        if value:
-            return value
-        return ''
-
     def _build_map(self, urls, parent_pattern=None):
         for url in urls.url_patterns:
-            url_pattern = "{}{}".format(self._if_none(parent_pattern), url.regex.pattern)
+            url_pattern = "{}{}".format(parent_pattern or '', url.regex.pattern)
             if isinstance(url, RegexURLResolver):
                 self._build_map(url, url_pattern)
             elif isinstance(url, RegexURLPattern):
-                _cls = self._import_by_path(url.lookup_str)
+                _cls = self._filter_rester_views(url.lookup_str)
                 if _cls:
                     self.urls_class_map.update({url_pattern: _cls})
