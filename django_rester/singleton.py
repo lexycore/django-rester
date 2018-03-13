27a6772a06083fda2e0946c49b7b29b78f47966a
zili <zili.tnd@gmail.com>
2018-03-13 22:10:53 +0300

    singleton threading lock

--- django_rester/singleton.py
+++ django_rester/singleton.py
@@ -1,7 +1,12 @@
+from threading import Lock
+
+
 class Singleton(type):
+    __singleton_lock = Lock()
     _instances = {}
 
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+            with cls.__singleton_lock:
+                cls._instances[cls] = super().__call__(*args, **kwargs)
         return cls._instances[cls]
